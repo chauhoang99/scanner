5704615bce60ff2379d56054fe87445961a95215
chauhoang99 <40813780+chauhoang99@users.noreply.github.com>
2026-09-10 13:18:10 +0800

    Change background color for bullish extreme styling

--- app.py
+++ app.py
@@ -330,7 +330,7 @@ def style_row(row):
             target_val = curr_val
             if target_val >= 3:
               styles[i] = (
-                  "background-color: ​​#00e676; color: #00bfff; font-weight:"
+                  "background-color: ​​lime; color: #00bfff; font-weight:"
                   " bold;"
               )
             else:
