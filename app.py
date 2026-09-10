40d95f2cfdc64c8e1304944ff7789bd9f27f30f0
chauhoang99 <40813780+chauhoang99@users.noreply.github.com>
2026-09-10 13:15:12 +0800

    Fix background color formatting in app.py

--- app.py
+++ app.py
@@ -330,7 +330,7 @@ def style_row(row):
             target_val = curr_val
             if target_val >= 3:
               styles[i] = (
-                  "background-color: ​​#00E676; color: #00bfff; font-weight:"
+                  "background-color: ​​#00e676; color: #00bfff; font-weight:"
                   " bold;"
               )
             else:
